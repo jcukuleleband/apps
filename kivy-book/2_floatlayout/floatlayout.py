31b8f5137d84b5be1c8c8944063d0dac9a5c76ce
jcukuleleband <jav.cortez@gmail.com>
2020-11-28 11:54:46 -0600

    Dev Machine learning updates

---
+++ kivy-book/2_floatlayout/floatlayout.py
@@ -0,0 +1,12 @@
+import kivy
+kivy.require('1.9.0')
+
+from kivy.app import App
+from kivy.uix.floatlayout import FloatLayout
+
+class FloatLayoutApp(App):
+    def build(self):
+        return FloatLayout()
+
+if  __name__=="__main__":
+    FloatLayoutApp().run()
